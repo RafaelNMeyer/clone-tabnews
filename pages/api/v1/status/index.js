2d4095392dce8d6fcddc3df9f25b470aab32a464
rafael.meyer <rafael.meyer@bry.com.br>
2023-12-19 09:09:44 -0300

    finish endpont `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js"
 
 async function status(request, response){
-  const result = await database.query('SELECT 1+1 as sum;');
-  console.log(result.rows)
-  response.status(200).json({status: 'ok'})
+  const updatedAt = new Date().toISOString();
+  
+  const dbVersion = await database.query('SHOW server_version;');
+  const dbVersionValue = dbVersion.rows[0].server_version;
+  
+  const maxConnections = await database.query('SHOW max_connections;');
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+  
+  const dbName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1; --AND state='active';",
+    values: [dbName]
+  });
+  const activeConnectionsValue = activeConnections.rows[0].count; 
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    db_version: dbVersionValue,
+    max_connections: parseInt(maxConnectionsValue),
+    active_connections: activeConnectionsValue
+  })
 }
 
 export default status;
