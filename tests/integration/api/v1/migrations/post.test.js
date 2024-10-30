8edbcfb5fea00b3741f03c3cb2d2545f90f4169a
RafaelNMeyer <rafaelniederauer07@gmail.com>
2024-10-29 22:49:17 -0300

    fix `prettier` style linting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,4 +1,4 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
@@ -7,9 +7,9 @@ beforeAll(async () => {
 });
 
 test("Post to /api/v1/migrations should return 200", async () => {
-  const response = await fetch('http://localhost:3000/api/v1/migrations',
-    { method: "POST" },
-  );
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response.status).toBe(201);
 
   const responseBody = await response.json();
@@ -18,9 +18,9 @@ test("Post to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 
-  const response2 = await fetch('http://localhost:3000/api/v1/migrations',
-    { method: "POST" },
-  );
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
   expect(response2.status).toBe(200);
 
   const responseBody2 = await response2.json();
@@ -28,5 +28,4 @@ test("Post to /api/v1/migrations should return 200", async () => {
 
   expect(Array.isArray(responseBody2)).toBe(true);
   expect(responseBody2.length).toBe(0);
-
 });
