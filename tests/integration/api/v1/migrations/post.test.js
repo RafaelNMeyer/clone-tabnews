d6aa0fff92b1bfafdef09b866a0f8dca7cc40a6d
RafaelNMeyer <rafaelniederauer07@gmail.com>
2024-11-09 20:06:46 -0300

    refactor: use orchestrator.clearDatabase() in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,18 +1,20 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
 describe("Post /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response.status).toBe(201);
 
         const responseBody = await response.json();
@@ -22,9 +24,12 @@ describe("Post /api/v1/migrations", () => {
       });
 
       test("For the second time", async () => {
-        const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-          method: "POST",
-        });
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
         expect(response2.status).toBe(200);
 
         const responseBody2 = await response2.json();
@@ -36,4 +41,3 @@ describe("Post /api/v1/migrations", () => {
     });
   });
 });
-
